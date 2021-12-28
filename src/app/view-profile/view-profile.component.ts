a8196f01dcfdaa4179f1048e58c7e1e2d52d6d0c
AswanthNarayanan <96513323+AswanthNarayanan@users.noreply.github.com>
2021-12-28 11:48:58 +0530

    updated

--- src/app/view-profile/view-profile.component.ts
+++ src/app/view-profile/view-profile.component.ts
@@ -17,6 +17,17 @@ export class ViewProfileComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+  }
 
 }
